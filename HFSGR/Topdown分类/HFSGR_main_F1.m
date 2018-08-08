df5de69f1269c5ed1edbf27766264eed3d1396f2
fhqxa <36149734+fhqxa@users.noreply.github.com>
2018-08-08 14:57:22 +0800

    Update HFSGR_main_F1.m

--- HFSGR/Topdown分类/HFSGR_main_F1.m
+++ HFSGR/Topdown分类/HFSGR_main_F1.m
@@ -1,28 +1,23 @@
 clear;clc;close all;
-%   title={Hierarchical feature selection with graph regularization based on subtree},
-%   author={Tuo, Qianjuan and Zhao, Hong and Hu, Qinghua},
-%% Load dataset
-%     load('ilsvrc65Train.mat')
-    load('CifarTrain.mat')
-%% initialization
-alpha =1;
-beita =0;
-Level_num  = max(tree(:,2));
-[~, numSelected] = size(data_array);
-numSelected = numSelected -1;
-%% Creat sub table
-[X, Y] = creatSubTablezh(data_array, tree);
-%% Feature selection
-  tic;
- [noLeafNode,F]=SubtreeRelationMatrix_F(tree);% Creat subtree relation matrix F
- [feature] = HFSGR_fast(X, Y, numSelected, tree, alpha, beita, noLeafNode, F, 1);
-  t=toc;
-%    save DDTrainfeature feature
 % %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Test feature
+% load('AWAphogTrainfeature_HiFSRR.mat')
+% load('VOCTrainfeature_HiFSRR.mat')
+% load('SUNTrainfeature_HiFSRR.mat')
+% load('CifarTrainfeature_HiFSRR.mat')
+% load('ilsvrc65Trainfeature_HiFSRR.mat')
+% load('Cifar100Trainfeature_HiFSRR.mat')
+%  load('DDTrainfeature_HiFSRR.mat')
+ load('F194Trainfeature_HiFSRR.mat')
 % %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% load('ilsvrc65Test.mat');begin1= 40;step1 = 40;end1 = 200;
-load('CifarTest.mat');begin1= 40;step1 = 40;end1 = 200;
+%  load('AWAphogTest.mat');begin1= 25;step1 = 25;end1 = 125;
+%  load('VOCTest.mat');begin1= 100;step1 = 100;end1 = 500;
+%  load ('SUNTest.mat');begin1= 410;step1 = 410;end1 = 2050;
+% load('ilsvrc65Test.mat');begin1= 410;step1 = 410;end1 = 1230;
+% load('Cifar100Test.mat');begin1= 410;step1 = 410;end1 = 1230;
+% load('CifarTest.mat');begin1= 50;step1 = 50;end1 = 250;
+% load('DDTest.mat');begin1= 48;step1 = 48;end1 = 144;
+load('F194Test.mat');begin1= 48;step1 = 48;end1 = 144;
 data_array = double(data_array);
  [M,N]=size(data_array);
  numFolds =10;
@@ -37,7 +32,15 @@ for j=begin1:step1:end1
     FHMean(k,1)=j;
     indices = crossvalind('Kfold',data_array(1:M,N),numFolds);
     [accuracyMean(k,ii),accuracyStd(k,ii),F_LCAMean(k,ii),FHMean(k,ii)] = FS_Kflod_TopDownClassifier(data_array,numFolds,tree,topDown,feature,j,indices);
-     fprintf(['A0.1--Accurate rate:',num2str(accuracyMean(k,ii)),'¡À', num2str(accuracyStd(k,ii)),'\n']);
+     fprintf(['A0.1--Accurate rate:',num2str(accuracyMean(k,ii)),'Â±', num2str(accuracyStd(k,ii)),'\n']);
     fprintf(['F_LCAMean:',num2str(F_LCAMean(k,ii)),'\nFHMean:', num2str(FHMean(k,ii)),'\n']);
     k=k+1;
 end
+% save AWAphog_HiFSRR_F1 FHMean
+% save VOC_HiFSRR_F1 FHMean
+% save SUN_HiFSRR_F1 FHMean
+% save Cifar_HiFSRR_F1 FHMean
+% save ilsvr65_HiFSRR_F1 FHMean
+% save Cifar100_HiFSRR_F1 FHMean
+% save DD_HiFSRR_F1 FHMean
+save F194_HiFSRR_F1 FHMean
